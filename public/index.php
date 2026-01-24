a27d7b9f75c888f52a3eeea807eda7d3b2d42f66
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-01-24 13:20:43 +0100

    add includes folder with view.php and templates with footer, header and index_get.php files, use the concept of layouts

--- public/index.php
+++ public/index.php
@@ -7,11 +7,14 @@ declare (strict_types = 1); //use strict types
 //zmienne dostępne w całym projekcie
 const INCLUDES_DIR = __DIR__ . '/../includes'; //__DIR__ zwraca rzeczywisty katalog; '/../" - one directory up
 const ROUTES_DIR = __DIR__ . '/../routes';
+const TEMPLATES_DIR = __DIR__ . '/../templates';
 
 //każde żądanie przychodzące do naszej aplikacji będzie przechodzić przez ten konkretny plik
 //jest więc dobrym miejscem na rozpoczęcie sesji:
 session_start();
-require_once INCLUDES_DIR . '/router.php'; //dołączenie pliku przy pomocy require, po rozpoczęciu sesji możemu użyć tego katalogu inlude i przekazać go routerowi PHP
+//dołączenie pliku przy pomocy require, po rozpoczęciu sesji możemu użyć tego katalogu inlude i przekazać go routerowi PHP
+require_once INCLUDES_DIR . '/router.php'; 
+require_once INCLUDES_DIR . '/view.php';
 
 //handle request -> obsłużenie żądania poprzez wywołanie funkcji routera
 dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);

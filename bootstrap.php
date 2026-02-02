e1d21addfcdf24029636eadd5dfc2159d1ad1036
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-02-02 17:34:31 +0100

    add error handling functions to handle exceptions and nonexception errors

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,16 @@
 <?php
 declare (strict_types = 1);
 
+require_once __DIR__ . '/error_handling.php';
+//obsługa błędów - dwa rodzaje
+//1 - to wywołanie tradycyjnego błędu np. wywołanie echo lub użycie zmiennej która nigdzie nie została zainicjowana
+error_reporting(E_ALL); //ustawienie poziomu raportowania błędów, aby wyświetlał wszystkie błędy
+//ini_set('display_errors', 1); - old version way, now is by default reported into console, not in the web page - za pomocą dowolnej metody użycie ustawienia o nazwie display unerscore errors
+//2- throw exeptions/wyjątki - możemy zgłaszać własne, albo obsługiwać wyjątki generowane przez niektóre biblioteki PHP
+//throw new RuntimeExeption('Whoops!');
+set_exception_handler('exceptionHandler');
+set_error_handler('errorHandler');
+
 //stałe dostępne w całym projekcie, wraz ze ścieżkami bezwzględnymi do katalogów używanych w całej aplikacji
 const INCLUDES_DIR = __DIR__ . '/includes'; //__DIR__ zwraca rzeczywisty katalog;
 const ROUTES_DIR = __DIR__ . '/routes';
--- routes/guestbook_get.php
+++ routes/guestbook_get.php
@@ -1,6 +1,8 @@
 <?php
 //pobranie wpisów z wykorzystaniem funkcji get messages i przekazanie im funkcji connect db - połączenie z bazą danych
 $messages = getMessages(connectDb());
+//throw new RunTimeException('Whoops!');
+echo $hey;
 //użycie funkcji widoku renderującego do przesłania danych
 renderView(
     template: 'guestbook_get', // nazwa szablonu wg konwencji jest taka sama jak nazwa routes
